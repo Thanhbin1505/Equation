8e3183ecf8f2666d012382e70135bd27cba936f1
Apple <apple@Apples-MacBook-Pro-2.local>
2013-09-29 22:44:05 +0700

    Initial Commit

---
+++ equation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  equation
+//
+//  Created by Apple on 9/29/13.
+//  Copyright (c) 2013 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ equation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  equation
+//
+//  Created by Apple on 9/29/13.
+//  Copyright (c) 2013 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
